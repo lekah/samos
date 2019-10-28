9701fe0cec531916601a19178fc8cdfd39d95133
Loris Ercole <loris.ercole@gmail.com>
2019-10-28 15:17:25 +0100

    Added __init__ files + version upgrade

--- samos/__init__.py
+++ samos/__init__.py
@@ -1,1 +1,7 @@
+from __future__ import absolute_import
 
+from .trajectory import Trajectory
+
+__all__ = ['Trajectory', 'analysis', 'io', 'lib', 'plotting', 'utils']
+
+from . import *
---
+++ samos/lib/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from .gaussian_density import *
+from .mdutils import *
+from .rdf import *
+
+__all__ = ['gaussian_density', 'mdutils', 'rdf']
+
+from . import *
