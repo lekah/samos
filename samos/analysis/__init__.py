aba48a655387ac1543a49de33d5172a08687e978
Leonid Kahle <lkahle@materialsdesign.com>
2023-07-19 14:18:47 +0200

    removed automatic imports in __init__

--- samos/analysis/__init__.py
+++ samos/analysis/__init__.py
@@ -1,8 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from .dynamics import TimeSeries, DynamicsAnalyzer
-from .rdf import BaseAnalyzer, RDF, AngularSpectrum
-
-__all__ = ['TimeSeries', 'DynamicsAnalyzer', 'BaseAnalyzer', 'RDF', 'AngularSpectrum', 'get_gaussian_density']
-
-from . import *
--- samos/plotting/__init__.py
+++ samos/plotting/__init__.py
@@ -1,6 +1,1 @@
 # -*- coding: utf-8 -*-
-
-__all__ = ['plot_dynamics', 'plot_rdf']
-# 'plot_xsf' requires mayavi.mlab
-
-from . import *
