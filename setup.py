022c2493d45b610beb3adabc50af35d07112ef65
Leonid Kahle <leonidkahle@gmail.com>
2026-03-09 14:12:10 +0100

    version update

--- setup.py
+++ setup.py
@@ -38,8 +38,48 @@ class f2py_Build(build_ext):
             os.system('cd %s;f2py -c %s -m %s' % (module_loc,to_compile,module_name))
 
 if __name__ == '__main__':
-    with open('setup.json', 'r') as info:
-        kwargs = json_load(info)
+    setup_kwargs = {
+        "author": "Leonid Kahle",
+        "author_email": "leonid.kahle@epfl.ch",
+        "classifiers": [
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Operating System :: OS Independent",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Topic :: Scientific/Engineering :: Chemistry",
+            "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+        "description": "Package for Analysis and Tricks for MOlecular Simulations: a set of useful tools to analyze molecular dynamics trajectoris",
+        "extras_require": {
+            "pre-commit": [
+                "pre-commit",
+                "flake8"
+            ],
+            "docs": [
+                "Sphinx",
+                "docutils",
+                "sphinx_rtd_theme"
+            ]
+        },
+        "install_requires": [
+            "numpy>=1.14.0",
+            "ase>=3.17.0",
+            "scipy>=1.0.0",
+            "matplotlib>=2.1.2"
+        ],
+        "license": "MIT License",
+        "name": "samos",
+        "url": "https://github.com/lekah/samos",
+        "version": "0.8"
+    }
+
     setup(
         include_package_data=True,
         packages=find_packages(),
@@ -47,5 +87,5 @@ if __name__ == '__main__':
         ## Following inexplicably works even though there is single name for all 3 f90 files
         ext_modules = [f2py_Extension('fortran_lib', ['samos/lib/gaussian_density.f90', 'samos/lib/mdutils.f90', 'samos/lib/rdf.f90'])],
         cmdclass=dict(build_ext=f2py_Build),
-        **kwargs
+        **setup_kwargs
     )
