75c1f3215c9fe69587de51d997cda4e4550727ea
Leonid Kahle <leonidkahle@gmail.com>
2026-03-09 15:04:56 +0100

    new setup

--- setup.py
+++ setup.py
@@ -1,41 +1,71 @@
-# -*- coding: utf-8 -*-
-
 import os
-from setuptools import find_packages, setup, Extension
-from json import load as json_load
+import sys
+import subprocess
+from setuptools import setup
 from setuptools.command.build_ext import build_ext
 
-ext1 = Extension(
-        name = 'samos.lib.gaussian_density',
-        sources = ['samos/lib/gaussian_density.f90'],
-    )
-ext2 = Extension(
-        name = 'samos.lib.mdutils',
-        sources = ['samos/lib/mdutils.f90'],
-    )
-ext3 = Extension(
-        name = 'samos.lib.rdf',
-        sources = ['samos/lib/rdf.f90'],
-    )
-
-class f2py_Extension(Extension):
-
-    def __init__(self, name, sourcedirs):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedirs = [os.path.abspath(sourcedir) for sourcedir in sourcedirs]
-        self.dirs = sourcedirs
-
-class f2py_Build(build_ext):
-
+class F2pyBuildExt(build_ext):
+    """Custom build command to compile Fortran files using f2py directly."""
+    
     def run(self):
-        for ext in self.extensions:
-            self.build_extension(ext)
+        # Ensure numpy is available
+        try:
+            import numpy
+            numpy_include = numpy.get_include()
+        except ImportError:
+            raise RuntimeError("NumPy must be installed to build extensions.")
+        
+        # We do NOT call super().run() here because that triggers the standard 
+        # Extension compilation logic which fails on .f90 files.
+        # Instead, we compile everything manually.
+        
+        # Define the modules to build
+        modules = [
+            ('samos.lib.gaussian_density', 'samos/lib/gaussian_density.f90'),
+            ('samos.lib.mdutils', 'samos/lib/mdutils.f90'),
+            ('samos.lib.rdf', 'samos/lib/rdf.f90'),
+        ]
+        
+        for module_name, source_file in modules:
+            self.build_module(module_name, source_file, numpy_include)
 
-    def build_extension(self, ext):
-        for index, to_compile in enumerate(ext.sourcedirs):
-            module_loc = os.path.split(ext.dirs[index])[0]
-            module_name = os.path.split(to_compile)[1].split('.')[0]
-            os.system('cd %s;f2py -c %s -m %s' % (module_loc,to_compile,module_name))
+    def build_module(self, module_name, source_file, numpy_include):
+        """Compile a single Fortran module using f2py."""
+        if not os.path.exists(source_file):
+            print(f"Warning: Source file {source_file} not found, skipping.")
+            return
+
+        source_dir = os.path.dirname(os.path.abspath(source_file))
+        module_short_name = module_name.split('.')[-1]
+        
+        # Construct the f2py command
+        # We use the system f2py module
+        cmd = [
+            sys.executable, "-m", "numpy.f2py",
+            "-c", source_file,
+            "-m", module_short_name,
+            f"--include-paths={numpy_include}",
+            "--f90flags=-fPIC"
+        ]
+        
+        # Change to the source directory to run f2py
+        old_cwd = os.getcwd()
+        try:
+            os.chdir(source_dir)
+            print(f"Compiling {source_file} -> {module_short_name}...")
+            subprocess.check_call(cmd)
+            
+            # f2py creates the .so file in the current directory (source_dir)
+            # We need to move it to the correct package location if building inplace
+            # or let setuptools handle the installation later.
+            # For 'build_ext --inplace', the .so ends up in source_dir, which is correct
+            # if source_dir is part of the package (samos/lib/).
+            
+        except subprocess.CalledProcessError as e:
+            print(f"Error compiling {source_file}: {e}")
+            raise
+        finally:
+            os.chdir(old_cwd)
 
 if __name__ == '__main__':
     setup_kwargs = {
@@ -44,10 +74,7 @@ if __name__ == '__main__':
         'classifiers': [
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
@@ -56,17 +83,10 @@ if __name__ == '__main__':
             'Topic :: Scientific/Engineering :: Chemistry',
             'Topic :: Software Development :: Libraries :: Python Modules'
         ],
-        'description': 'Package for Analysis and Tricks for MOlecular Simulations: a set of useful tools to analyze molecular dynamics trajectoris',
+        'description': 'Package for Analysis and Tricks for MOlecular Simulations',
         'extras_require': {
-            'pre-commit': [
-                'pre-commit',
-                'flake8'
-            ],
-            'docs': [
-                'Sphinx',
-                'docutils',
-                'sphinx_rtd_theme'
-            ]
+            'pre-commit': ['pre-commit', 'flake8'],
+            'docs': ['Sphinx', 'docutils', 'sphinx_rtd_theme']
         },
         'install_requires': [
             'numpy>=1.14.0',
@@ -77,15 +97,12 @@ if __name__ == '__main__':
         'license': 'MIT License',
         'name': 'samos',
         'url': 'https://github.com/lekah/samos',
-        'version': '0.8'
+        'version': '0.8',
+        'packages': ['samos', 'samos.lib'],
+        'package_data': {'samos.lib': ['*.f90']},
+        # IMPORTANT: Do NOT include ext_modules here. 
+        # The custom build command handles everything.
+        'cmdclass': {'build_ext': F2pyBuildExt},
     }
 
-    setup(
-        include_package_data=True,
-        packages=find_packages(),
-        package_data = {'': ['*.f90']},
-        ## Following inexplicably works even though there is single name for all 3 f90 files
-        ext_modules = [f2py_Extension('fortran_lib', ['samos/lib/gaussian_density.f90', 'samos/lib/mdutils.f90', 'samos/lib/rdf.f90'])],
-        cmdclass=dict(build_ext=f2py_Build),
-        **setup_kwargs
-    )
+    setup(**setup_kwargs)
